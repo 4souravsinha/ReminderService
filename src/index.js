252978da15dbc56e481830947c4cec537e33665b
4souravsinha <4souravsinhain@gmail.com>
2023-02-20 17:50:42 +0530

    setup of smtp using nodemailer done

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 
 const { PORT } = require('./config/serverConfig')
-
+const { sendBasicEmail } = require('./services/email-service')
 
 const setupAndStartServer = () => {
     const app = express()
@@ -11,6 +11,14 @@ const setupAndStartServer = () => {
 
     app.listen(PORT, () => {
         console.log(`Reminder Service up and running at port ${PORT}`)
+
+        // sendBasicEmail(
+        //     'support@am.in',
+        //     '1929122@kiit.ac.in',
+        //     'You have been selected at amazon for SDE role',
+        //     'Congrats , submit your document'
+        // )
+
     })
 }
 
