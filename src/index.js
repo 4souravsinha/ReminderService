328e1136869a85d8967fb940713b3e29b59bcc37
4souravsinha <4souravsinhain@gmail.com>
2023-02-21 23:02:06 +0530

    completed notification cron
    completed notification cron to send email and update status

--- src/index.js
+++ src/index.js
@@ -3,16 +3,18 @@ const bodyParser = require('body-parser')
 
 const { PORT } = require('./config/serverConfig')
 // const { sendBasicEmail } = require('./services/email-service')
-var cron = require('node-cron');
+const jobs = require('./utils/job')
+const TicketController = require('./controllers/ticket-controller')
 
 const setupAndStartServer = () => {
     const app = express()
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: true }))
 
+    app.post('/api/v1/tickets' , TicketController.create);
+    jobs();
     app.listen(PORT, () => {
         console.log(`Reminder Service up and running at port ${PORT}`)
-
 
     })
 
